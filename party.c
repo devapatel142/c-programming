75528ab6bf68708376b8bd0f48574f941321ff51
devapatel142 <essampallydeva142@gmail.com>
2026-02-11 14:58:42 +0530

    Update party.c

--- party.c
+++ party.c
@@ -4,16 +4,17 @@ int main() {
    int tea,candy;
    printf("enter the amount party of tea and candy.\n");
    scanf(" %d %d",&tea,&candy);
-   if(tea<=5||candy<=5)
+   if(tea<5||candy<5)
    {
 printf("0");
 }
-else if(tea>=5&&candy>=5)
+else if(tea>=2*candy||candy>=2*tea)
 {
-    printf("1");
-}
-else{
     printf("2");
 }
+else{
+    printf("1");
+}
     return 0;
+
 }
